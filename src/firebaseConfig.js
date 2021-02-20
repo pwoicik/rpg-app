0f7ebd0fc430310e512a4cc23847cbd278d4c702
pwoicik <patrykwoicik@gmail.com>
2021-02-20 16:08:06 +0100

    Initial commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDUkBGgATOQaj4l84VUMLd0rJVOKaDIB98",
+  authDomain: "rpg-app-6a038.firebaseapp.com",
+  projectId: "rpg-app-6a038",
+  storageBucket: "rpg-app-6a038.appspot.com",
+  messagingSenderId: "917447980725",
+  appId: "1:917447980725:web:40de81268959ff0eb830fc"
+};
